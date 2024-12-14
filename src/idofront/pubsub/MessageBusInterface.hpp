cff08d1c646613de91ca54b2895daf51e8088c47
idofront <139764549+idofront@users.noreply.github.com>
2024-12-14 14:18:50 +0900

    Implement Multi-Channel MessageBus (#8)
    * Implement Multi-Channel MessageBus
    
    * ビルド時の警告強化
    
    * MessageBusExample を更新

--- src/idofront/pubsub/MessageBusInterface.hpp
+++ src/idofront/pubsub/MessageBusInterface.hpp
@@ -17,17 +17,17 @@ template <typename T> class MessageBusInterface
 
     /// @brief Publish a message
     /// @param message Message to publish
-    virtual void Publish(T message) = 0;
+    virtual void Publish(T message, const std::string &topic = "") = 0;
 
     /// @brief Subscribe to the message bus
     /// @param subscriber Subscriber function
     /// @return Stub to the subscriber
-    virtual ticket::Stub Subscribe(std::function<void(T)> subscriber) = 0;
+    virtual ticket::Stub Subscribe(std::function<void(T)> subscriber, const std::string &topic = "") = 0;
 
     /// @brief Unsubscribe from the message bus
     /// @param stub Stub to unsubscribe
     /// @return True if the subscriber was unsubscribed, false otherwise
-    virtual bool Unsubscribe(ticket::Stub stub) = 0;
+    virtual bool Unsubscribe(ticket::Stub stub, const std::string &topic = "") = 0;
 };
 } // namespace pubsub
 } // namespace idofront
