b61f0a960d2de038ada7cb5595c8a10466bbb924
Kumazawa <kumazawa@ic.idofront.com>
2024-12-22 00:34:21 +0900

    class Parser の追加

--- main/ArgumentExample.cpp
+++ main/ArgumentExample.cpp
@@ -5,28 +5,28 @@ int main(int argc, char *argv[])
 {
     try
     {
+        auto parser = idofront::argument::Parser(argc, argv);
+
         auto lengthArgument = idofront::argument::Argument<uint8_t>::New('l', "length")
                                   .Description("The length of the manager.")
                                   .Default(1);
-        auto length = idofront::argument::Parse<uint8_t>(argc, argv, lengthArgument);
+        auto length = parser.Parse(lengthArgument);
+
         auto messageArgument = idofront::argument::Argument<std::string>::New("message")
                                    .Description("The message to be published.")
                                    .Default("Hello, world!");
-        auto message = idofront::argument::Parse<std::string>(argc, argv, messageArgument);
-        auto help = idofront::argument::Parse<idofront::argument::type::Flag>(argc, argv, idofront::argument::Help);
+        auto message = parser.Parse(messageArgument);
 
-        if (help.value())
+        auto help = parser.Parse<idofront::argument::type::Flag>(idofront::argument::Help);
+
+        if (help.Value().value())
         {
-            std::cout << "Usage: " << argv[0] << " [options]\n";
-            std::cout << "Options:\n";
-            std::cout << lengthArgument.ToHelpString() << "\n";
-            std::cout << messageArgument.ToHelpString() << "\n";
-            std::cout << idofront::argument::Help.ToHelpString() << "\n";
-            return 1;
+            parser.ShowHelp();
+            return 0;
         }
 
-        std::cout << "Length: " << (uint64_t)length.value() << "\n" << std::flush;
-        std::cout << "Message: " << message.value() << "\n" << std::flush;
+        std::cout << "Length: " << (uint64_t)length.Value().value() << "\n" << std::flush;
+        std::cout << "Message: " << message.Value().value() << "\n" << std::flush;
     }
     catch (const std::exception &e)
     {
